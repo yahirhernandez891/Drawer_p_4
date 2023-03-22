b1edf083ca30838f73017e63e1263ca87c18712a
YAHIR_HIRAM_HERNANDEZ_GARCIA <a.20308051280619@cbtis128.edu.mx>
2023-03-22 18:21:58 +0000

    drawer_v3

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  static const String _title = 'Flutter Stateful Clicker Counter';
-  // This widget is the root of your application.
+  static const String _title = 'Yaya Drawer';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,10 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state.
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -32,12 +27,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -46,24 +35,23 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       drawer: Drawer(
         child: ListView(
-          // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: [
             const UserAccountsDrawerHeader(
-              // <-- SEE HERE
               decoration: BoxDecoration(color: const Color(0xffd50071)),
               accountName: Text(
-                "Encabezado",
+                "Yahir Hiram Hernández 6J",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                 ),
               ),
               accountEmail: Text(
-                "Correo del usuario",
+                "a.20308051280619@cbtis128.edu.mx",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              currentAccountPicture: FlutterLogo(),
             ),
             ListTile(
               leading: Icon(
